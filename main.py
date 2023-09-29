f6f57d8cd7aff7c95d9c55a1c806ca40bac3cb3c
maicon amancio <maicon.w.a@gmail.com>
2023-09-29 19:57:04 -0300

    Add game objects

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
+# game objects
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+cpu_opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
+
 # runs the game
 while True:
     # input handler
