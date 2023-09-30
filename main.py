888377a1c304d9f6e6937813da6a1a2bb0e67218
maicon amancio <maicon.w.a@gmail.com>
2023-09-30 07:00:34 -0300

    improved collisions and sound

--- main.py
+++ main.py
@@ -12,19 +12,37 @@ def ball_animation():
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
+        pygame.mixer.Sound.play(pong_sound)
         ball_speed_y *= -1
 
     if ball.left <= 0:
+        pygame.mixer.Sound.play(score_sound)
         player_score += 1
         score_time = pygame.time.get_ticks()
 
     if ball.right >= screen_width:
+        pygame.mixer.Sound.play(score_sound)
         cpu_opponent_score += 1
         score_time = pygame.time.get_ticks()
 
     # game collision
-    if ball.colliderect(player) or ball.colliderect(cpu_opponent):
-        ball_speed_x *= -1
+    if ball.colliderect(player) and ball_speed_x > 0:
+        pygame.mixer.Sound.play(pong_sound)
+        if abs(ball.right - player.left) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - player.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - player.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
+
+    if ball.colliderect(cpu_opponent) and ball_speed_x < 0:
+        pygame.mixer.Sound.play(pong_sound)
+        if abs(ball.left - cpu_opponent.left) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.bottom - cpu_opponent.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+        elif abs(ball.top - cpu_opponent.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
 
 
 def player_animation():
@@ -81,6 +99,7 @@ def ball_start():
 
 
 # setup
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -109,6 +128,10 @@ cpu_opponent_speed = 7
 player_score = 0
 cpu_opponent_score = 0
 game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+# sound
+pong_sound = pygame.mixer.Sound("pong.ogg")
+score_sound = pygame.mixer.Sound("score.ogg")
 
 # timer
 score_time = True
